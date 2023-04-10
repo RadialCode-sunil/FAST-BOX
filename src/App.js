370a5487e46c07af4aace1a819b0dd09f086793c
sunil-170 <sunilkhokha015@gmail.com>
2023-04-10 15:15:15 +0530

    fg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import { Navbar } from 'react-bootstrap';
+import { Calculate } from './components/Calculate';
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Calculate/>
     </div>
   );
 }
