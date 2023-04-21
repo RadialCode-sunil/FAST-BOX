643f1e472cd6a189589105295f390f4af9c8e759
RadialCode-sunil <sunilbatesar0@gmail.com>
2023-04-21 10:28:58 +0530

    footer

--- src/components/Airtable.jsx
+++ src/components/Airtable.jsx
@@ -7,32 +7,34 @@ import slack from '../assets/img/png/slack.png';
 import licchat from '../assets/img/png/licchat.png';
 import gitlab from '../assets/img/png/gitlab.png'
 const Airtable = () => {
-  return (
-      <section className=' bg_lightgreen3 py-5'>
-          <Container>
-              <Row className=' justify-content-center'>
-                  <Col xs={6} sm={4} md={3} lg={2}>
-                      <img className=' w-75 logo_w_xs logo_filter' src={airtable} alt="Airtable" />
-                  </Col>
-                  <Col xs={6} sm={4} md={3} lg={2}>
-                      <img className=' w-75 logo_w_xs logo_filter' src={sketch} alt="Sketch" />
-                  </Col>
-                  <Col xs={6} sm={4} md={3} lg={2} className=' pt-3 pt-sm-0'>
-                      <img className=' w-75 logo_w_xs logo_filter' src={dribble} alt="Dribble" />
-                  </Col>
-                  <Col xs={6} sm={4} md={3} lg={2} className=' pt-3 pt-md-0'>
-                      <img className=' w-75 logo_w_xs logo_filter' src={slack} alt="Slack" />
-                  </Col>
-                  <Col xs={6} sm={4} md={3} lg={2} className=' pt-3 pt-lg-0'>
-                      <img className=' w-75 logo_w_xs logo_filter' src={licchat} alt="LiveChat" />
-                  </Col>
-                  <Col xs={6} sm={4} md={3} lg={2} className=' pt-3 pt-lg-0'>
-                      <img className=' w-75 logo_w_xs logo_filter' src={gitlab} alt="Gitlab" />
-                  </Col>
-              </Row>
-          </Container>
-    </section>
-  )
+    return (
+        <section className=' bg_lightgreen3 py-5'>
+            <Container>
+                <Row className=' justify-content-center'>
+                    <Col xs={6} sm={4} md={3} lg={2}>
+                        <a href="#"><img className=' w-75 logo_w_xs logo_filter' src={airtable} alt="Airtable" />
+                        </a>
+                    </Col>
+                    <Col xs={6} sm={4} md={3} lg={2}>
+                        <a href="#"><img className=' w-75 logo_w_xs logo_filter' src={sketch} alt="Sketch" />
+                        </a>
+                    </Col>
+                    <Col xs={6} sm={4} md={3} lg={2} className=' pt-3 pt-sm-0'>
+                        <a href="#"><img className=' w-75 logo_w_xs logo_filter' src={dribble} alt="Dribble" /></a>
+                    </Col>
+                    <Col xs={6} sm={4} md={3} lg={2} className=' pt-3 pt-md-0'>
+                        <a href="#"><img className=' w-75 logo_w_xs logo_filter' src={slack} alt="Slack" /></a>
+                    </Col>
+                    <Col xs={6} sm={4} md={3} lg={2} className=' pt-3 pt-lg-0'>
+                        <a href="#"><img className=' w-75 logo_w_xs logo_filter' src={licchat} alt="LiveChat" /></a>
+                    </Col>
+                    <Col xs={6} sm={4} md={3} lg={2} className=' pt-3 pt-lg-0'>
+                        <a href="#"><img className=' w-75 logo_w_xs logo_filter' src={gitlab} alt="Gitlab" /></a>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 }
 
 export default Airtable
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,36 +8,36 @@ const Footer = () => {
         <section>
             <Container>
                 <Row>
-                    <Col xs={12} lg={5}>
+                    <Col xs={12} xl={4}>
                         <a href="#"><img src={logo} alt="logo" /></a>
-                        <p className=' pe-5 me-5 ff_Red_Hat fw_400 fs_sm grey90 pt-4'>
+                        <p className='ff_Red_Hat fw_400 fs_sm grey90 pt-4'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam..</p>
                     </Col>
-                    <Col xs={12} lg={7}>
+                    <Col xs={12} xl={8}>
                         <Row>
-                            <Col lg={4}>
-                                <div className=' d-flex justify-content-center quick_link position-relative'>
+                            <Col xs={12} sm={6} xxl={4}>
+                                <div className=' d-flex justify-content-xl-center quick_link position-relative'>
                                     <ul className=' list-unstyled  '>
                                         <li className=' ff_Anybody fw_700 fs_lg darkgreen'>Quick link</li>
-                                        <li><a className=' ff_Red_Hat fw_400 fs_sm grey90 d-inline-block pt-5' href="#">Tracking</a></li>
+                                        <li><a className=' ff_Red_Hat fw_400 fs_sm grey90 d-inline-block pt-3 pt-md-4 pt-lg-5' href="#">Tracking</a></li>
                                         <li><a className=' ff_Red_Hat fw_400 fs_sm grey90 pt-3 d-inline-block' href="#">Shipping</a></li>
                                         <li><a className=' ff_Red_Hat fw_400 fs_sm grey90 pt-3 d-inline-block' href="#">Locations</a></li>
                                         <li><a className=' ff_Red_Hat fw_400 fs_sm grey90 pt-3 d-inline-block' href="#">Support</a></li>
                                     </ul>
                                 </div>
                             </Col>
-                            <Col lg={4}>
-                                <div className=' d-flex justify-content-center quick_border position-relative'>
+                            <Col xs={12} sm={6} xxl={4}>
+                                <div className=' d-flex justify-content-xl-center quick_border position-relative'>
                                     <ul className=' list-unstyled'>
                                         <li className=' ff_Anybody fw_700 fs_lg darkgreen'>Contact us</li>
-                                        <div className=' d-flex pt-5 mt-2'>
+                                        <div className=' d-flex mt-3 mt-md-4 mt-lg-5 mt-lg-2'>
                                             <span><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M14.259 10.9467V12.9467C14.2597 13.1324 14.2217 13.3162 14.1473 13.4863C14.0729 13.6564 13.9638 13.8091 13.827 13.9346C13.6902 14.0602 13.5287 14.1557 13.3528 14.2152C13.1769 14.2747 12.9905 14.2968 12.8056 14.2801C10.7542 14.0572 8.78362 13.3562 7.05229 12.2334C5.4415 11.2098 4.07584 9.84418 3.05229 8.2334C1.9256 6.4942 1.22445 4.51406 1.00562 2.4534C0.988959 2.26904 1.01087 2.08324 1.06995 1.90781C1.12904 1.73239 1.224 1.57119 1.3488 1.43448C1.47359 1.29777 1.62549 1.18854 1.79481 1.11375C1.96414 1.03895 2.14718 1.00024 2.33229 1.00006H4.33229C4.65582 0.99688 4.96948 1.11145 5.21479 1.32242C5.46011 1.53339 5.62034 1.82636 5.66562 2.14673C5.75003 2.78678 5.90659 3.41522 6.13229 4.02006C6.22198 4.25868 6.24139 4.51801 6.18822 4.76732C6.13505 5.01663 6.01153 5.24547 5.83229 5.42673L4.98562 6.2734C5.93466 7.94243 7.31659 9.32436 8.98562 10.2734L9.83229 9.42673C10.0135 9.24749 10.2424 9.12396 10.4917 9.07079C10.741 9.01762 11.0003 9.03704 11.239 9.12673C11.8438 9.35243 12.4722 9.50898 13.1123 9.5934C13.4361 9.63909 13.7319 9.8022 13.9433 10.0517C14.1547 10.3013 14.2671 10.6198 14.259 10.9467Z" stroke="#777E90" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg>
                                             </span><li><a className=' ps-3 ff_Red_Hat fw_400 fs_sm grey90' href="#">(209) 555-0104</a></li>
                                         </div>
-                                        <div className=' d-flex pt-4'>
+                                        <div className=' d-flex pt-2 pt-lg-4'>
                                             <span><svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M14.75 14.75H3.5C2.125 14.75 1 13.625 1 12.25V3.5C1 2.125 2.125 1 3.5 1H14.75C16.125 1 17.25 2.125 17.25 3.5V12.25C17.25 13.625 16.125 14.75 14.75 14.75Z" stroke="#777E90" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                                                 <path d="M1 6L9.125 11L17.25 6" stroke="#777E90" stroke-width="1.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
@@ -45,7 +45,7 @@ const Footer = () => {
                                             </span>
                                             <li><a className=' ps-3 ff_Red_Hat fw_400 fs_sm grey90 ' href="#">michelle.rivera@example.com</a></li>
                                         </div>
-                                        <div className=' d-flex pt-4'>
+                                        <div className=' d-flex pt-2 pt-lg-4'>
                                             <span><svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M6.99785 0.622559C3.2083 0.622559 0.123464 3.7074 0.123463 7.49694C0.12346 14.1649 6.6103 19.2341 6.6103 19.2341C6.72103 19.3225 6.85849 19.3706 7.00015 19.3706C7.14182 19.3706 7.27928 19.3225 7.39001 19.2341C7.39001 19.2341 13.8769 14.1649 13.8768 7.49694C13.8768 3.7074 10.7874 0.622559 6.99785 0.622559ZM6.99785 1.87287C10.1118 1.87287 12.6265 4.38294 12.6265 7.49694C12.6265 12.9519 7.68702 17.2617 7.00246 17.8454C6.32117 17.2646 1.37377 12.954 1.37377 7.49694C1.37377 4.38294 3.88385 1.87287 6.99785 1.87287Z" fill="#777E90" />
                                                 <path d="M6.99785 4.37354C5.27936 4.37354 3.87439 5.77852 3.87439 7.497C3.87438 9.2155 5.27935 10.6251 6.99785 10.6251C8.71634 10.6251 10.1259 9.2155 10.1259 7.497C10.1259 5.77852 8.71633 4.37354 6.99785 4.37354ZM6.99785 5.62384C8.04078 5.62384 8.87561 6.45406 8.87562 7.497C8.87562 8.53993 8.04079 9.37476 6.99785 9.37476C5.95491 9.37476 5.12469 8.53993 5.12469 7.497C5.1247 6.45406 5.95491 5.62384 6.99785 5.62384Z" fill="#777E90" />
@@ -58,13 +58,13 @@ const Footer = () => {
                                     </ul>
                                 </div>
                             </Col>
-                            <Col lg={4}>
-                                <div className=''>
+                            <Col xs={12} sm={6} xxl={4}>
+                                <div className=' d-flex justify-content-xxl-center ps-xxl-4 pt-4 pt-xl-0'>
                                     <ul className='list-unstyled'>
                                         <li className=' ff_Anybody fw_700 fs_lg darkgreen'>Subscribe</li>
-                                        <input className=' input_txt mt-5' type="text" placeholder='Name' />
+                                        <input className=' input_txt mt-3 mt-md-4 mt-lg-5' type="text" placeholder='Name' /> <br />
                                         <input className=' mt-4 input_txt mt-4' type="text" placeholder='Email' />
-                                        <div className=' pt-5 mt-5'>
+                                        <div className='pt-3 pt-md-5 mt-xl-5'>
                                             <Btn_green any_text='Subscribe' />
                                         </div>
                                     </ul>
@@ -74,9 +74,9 @@ const Footer = () => {
                     </Col>
                 </Row>
                 <div className='footer_border mt-3'></div>
-                <div className=' d-flex justify-content-between pt-5'>
+                <div className=' d-flex flex-column flex-lg-row justify-content-between pt-5'>
                     <p className=' ff_Red_Hat fw_400 fs_lg grey90'>Copyright 2023 | Uitaskca - All rights Reserved</p>
-                    <div className=''>
+                    <div className=' pb-3'>
                         <span className=' pe-5'><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M10 16.6667C13.6819 16.6667 16.6667 13.682 16.6667 10.0001C16.6667 6.31818 13.6819 3.33341 10 3.33341C6.31812 3.33341 3.33335 6.31818 3.33335 10.0001C3.33335 13.682 6.31812 16.6667 10 16.6667ZM10 18.3334C14.6024 18.3334 18.3334 14.6024 18.3334 10.0001C18.3334 5.39771 14.6024 1.66675 10 1.66675C5.39765 1.66675 1.66669 5.39771 1.66669 10.0001C1.66669 14.6024 5.39765 18.3334 10 18.3334Z" fill="#777E91" />
                             <path d="M10 8.33325C10 7.87302 10.3731 7.49992 10.8334 7.49992H11.6667C12.1269 7.49992 12.5 7.12683 12.5 6.66659C12.5 6.20635 12.1269 5.83325 11.6667 5.83325H10.8334C9.4526 5.83325 8.33335 6.95254 8.33335 8.33325V9.99992H7.50002C7.03979 9.99992 6.66669 10.373 6.66669 10.8333C6.66669 11.2935 7.03978 11.6666 7.50002 11.6666H8.33335V16.6666C8.33335 17.1268 8.70644 17.4999 9.16669 17.4999C9.62694 17.4999 10 17.1268 10 16.6666V11.6666H11.6667C12.1269 11.6666 12.5 11.2935 12.5 10.8333C12.5 10.373 12.1269 9.99992 11.6667 9.99992H10V8.33325Z" fill="#777E91" />
